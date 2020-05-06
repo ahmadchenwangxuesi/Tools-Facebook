09751e06b3edce5386eea2b0b3450a0f5f9089e4
Rootpaceusa <64696314+ahmadchenwangxuesi@users.noreply.github.com>
2020-05-06 20:18:30 +0700

    Create random.php

---
+++ modules/random.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * If you are a reliable programmer or the best developer, please don't change anything.
+ * If you want to be appreciated by others, then don't change anything in this script.
+ * Please respect me for making this tool from the beginning.
+ */
+$n=1; 
+function getName($n) { 
+    $characters = '123456789'; 
+    $randomString = ''; 
+  
+    for ($i = 0; $i < $n; $i++) { 
+        $index = rand(0, strlen($characters) - 1); 
+        $randomString .= $characters[$index]; 
+    } 
+  
+    return $randomString; 
+} 
+
+/**
+ * 
+ * If you are a reliable programmer or the best developer, please don't change anything.
+ * If you want to be appreciated by others, then don't change anything in this script.
+ * Please respect me for making this tool from the beginning.
+ */
